11c903eda1f8326faeb25be3a22d61852768cc9c
Spas Z. Spasov <spas.z.spasov@metalevel.tech>
2023-02-26 22:26:12 +0200

    Migrate to Laravel&React/TS/TSX

---
+++ resources/ts/Pages/Welcome.tsx
@@ -0,0 +1,12 @@
+import { Link, Head } from "@inertiajs/react";
+
+interface Props {}
+
+export default function Welcome(props: Props) {
+  return (
+    <>
+      <Head title="Welcome" />
+      Test
+    </>
+  );
+}
